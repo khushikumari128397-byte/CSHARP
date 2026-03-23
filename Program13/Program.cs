c06e14afa1c4b6e5631d7f2c3382ba1ab5e50822
khushikumari128397-byte <khushikumari.128397@marwadiuniversity.ac.in>
2026-03-23 11:56:13 +0530

    Add files via upload

---
+++ Program13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, min;
+
+            Console.WriteLine("Enter first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter third number: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            min = (a < b) ?
+                   ((a < c) ? a : c)
+                   :
+                   ((b < c)  ? b : c);
+
+            Console.WriteLine("Minimum number is: " + min);
+                    
+        }
+    }
+}
