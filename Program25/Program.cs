bbcfe838ca502171588b877099bdfc307ead35d6
khushikumari128397-byte <khushikumari.128397@marwadiuniversity.ac.in>
2026-04-10 16:25:46 +0530

    Add files via upload

---
+++ Program25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Result r = new Result();
+
+            Console.WriteLine(r.Pass(55));
+            Console.WriteLine(r.Division(58));
+        }
+    }
+}
---
+++ Program25/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program26
+{
+    class Result : Exam, Classify
+    {
+        public bool Pass(int mark)
+        {
+            return mark >= 50;
+        }
+
+        public string Division(int average)
+        {
+            if (average >= 60)
+                return "First";
+            else if (average >= 50)
+                return "Second";
+            else
+                return "No division";
+        }
+    }
+}
